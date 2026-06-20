81d5d201bccd7c4c400abff8b3d4341cbdc33d07
maixuzi <635116278@qq.com>
2026-06-20 16:37:13 +0800

    Add Decap CMS admin

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #layout-bottom>
+      <a class="mxz-admin-link" href="/admin/" target="_blank" rel="noopener" title="内容管理后台">⚙</a>
+    </template>
+  </Layout>
+</template>
