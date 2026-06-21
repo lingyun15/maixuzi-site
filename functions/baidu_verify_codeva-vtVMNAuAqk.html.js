97e800044e5bc47f09d4d55c01c2e276259b7467
maixuzi <635116278@qq.com>
2026-06-21 16:44:57 +0800

    Fix Baidu verification exact html path

---
+++ functions/baidu_verify_codeva-vtVMNAuAqk.html.js
@@ -0,0 +1,8 @@
+export function onRequestGet() {
+  return new Response('a8b3c0dc0136e1d11b39b6b0b589e149', {
+    headers: {
+      'content-type': 'text/html; charset=utf-8',
+      'cache-control': 'no-store'
+    }
+  })
+}
